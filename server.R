447a62b684535571fe0a9cdc6ccf6b69c964aec8
Steve Nay <steve@scnay.com>
2016-01-27 17:10:42 -0700

    Improve help text and add a graph of the function

--- server.R
+++ server.R
@@ -15,9 +15,20 @@ shinyServer(function(input, output) {
       second <- (-b - term2)/2*a
       
       paste('Solutions are', first, 'and', second)
+    } else if (b == 0 && sqrt_term == 0) {
+      paste('The solution is 0')
     } else {
       paste('Since the square root term is negative, there are no real solutions.')
     }
+  })
+  # Graph the formula
+  output$graph <- renderPlot({
+    a <- as.numeric(input$a)
+    b <- as.numeric(input$b)
+    c <- as.numeric(input$c)
+    curve(a*x^2 + b*x + c, from=-5, to=5, ylim=c(-5,5), col='red', lwd=2)
+    abline(h=0)
+    abline(v=0)
   })
   
 })
--- ui.R
+++ ui.R
@@ -7,15 +7,18 @@ shinyUI(pageWithSidebar(
   
   # Sidebar with a text inputs for the terms
   sidebarPanel(
-    helpText('Enter the terms a, b, and c to calculate the solutions for the quadratic formula'),
+    helpText('Enter the coefficients a, b, and c from a quadratic formula y=a*x^2 + b*x + c, and we will calculate the roots of the parabola via the quadratic formula.'),
     numericInput('a','a',1),
-    numericInput('b','b',3),
-    numericInput('c','c',2)
+    numericInput('b','b',0),
+    numericInput('c','c',0)
   ),
   
   # Display the answer
   mainPanel(
-    helpText('The formula is (-b +/- sqrt(b^2 -4ac))/2a'),
-    textOutput('solution')
+    helpText('The quadratic formula is (-b +/- sqrt(b^2 -4ac))/2a'),
+    helpText('Solving that given your coefficients, we get this:'),
+    textOutput('solution'),
+    helpText('For reference, the graph of your function is this:'),
+    plotOutput('graph')
   )
 ))
